5cedca9008dbc4adca3e4bac555286ac40c0aefd
Brendan-Z <brendanzhou.99@gmail.com>
2022-09-30 12:45:12 +1300

    merge main

---
+++ src/test/java/IO/TestInputParsing.java
@@ -0,0 +1,34 @@
+package IO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestInputParsing {
+
+    @Test
+    void testBasicArgs() {
+        String[] args = { "testBasicArgs.dot" };
+
+        InputCommand actual = CLIParser.commandLineParser(args);
+
+        assertAll(
+                "Tests that the input file name and number of processors are correct",
+                () -> assertEquals("testBasicArgs.dot", actual.getInputFile())
+        );
+    }
+
+    @Test
+    void testDefaultOutputFile() {
+        String[] args = { "testDefaultOutputFile.dot" };
+
+        InputCommand actual = CLIParser.commandLineParser(args);
+
+        assertEquals("testDefaultOutputFile-output.dot",
+                actual.getOutputFile());
+        System.out.print(actual.getOutputFile());
+
+    }
+}
