f8ce3842351b33583a91e89a0f9f88d96f564f75
arim402 <arim402@aucklanduni.ac.nz>
2022-10-05 16:55:43 +1300

    Implement timer

---
+++ src/main/java/visualisation/UITimer.java
@@ -0,0 +1,38 @@
+package visualisation;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class UITimer extends Thread {
+    private int counter;
+    private VisualizationController _controller;
+    private Timer timer;
+
+    public void setController(VisualizationController controller) {
+        _controller = controller;
+    }
+
+    /**
+     * Called by {@link VisualizationController} startAction() to start timer
+     */
+    public void startUITimer() {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                counter++;
+                if (_controller != null) {
+                    _controller.setUITimer(counter);
+                }
+            }
+        };
+        timer = new Timer("timer");
+        timer.scheduleAtFixedRate(timerTask, 0, 10);
+    }
+
+    /**
+     * Called by {@link VisualizationController} stopAction() to stop timer
+     */
+    public void stopUITimer() {
+        timer.cancel();
+    }
+}
--- src/main/java/visualisation/VisualizationController.java
+++ src/main/java/visualisation/VisualizationController.java
@@ -1,5 +1,69 @@
 package visualisation;
 
-public class VisualizationController {
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
+public class VisualizationController {
+    private static UITimer timer;
+
+    @FXML
+    private Label timerLabel;
+
+    /**
+     *  Initialises components of the JavaFX window for visualisation
+     */
+    @FXML
+    public void initialize() {
+        timer = new UITimer();
+        timer.setController(this);
+    }
+
+    /**
+     * Called by {@link UITimer} startUITimer() to update timer label
+     * @param counter incremented value
+     */
+    @FXML
+    public synchronized void setUITimer(int counter) {
+        Platform.runLater(() -> {
+            String minEmpty = "";
+            String secEmpty = "";
+            String msecEmpty = "";
+            // Split counter into relevant parts of timer format
+            long minutes = counter / 6000;
+            long seconds = (counter - minutes * 6000) / 100;
+            long milliseconds = counter - minutes * 6000 - seconds * 100;
+
+            // 0 to be appended to the front if the value is less than 10
+            if (minutes < 10) {
+                minEmpty = "0";
+            }
+            if (seconds < 10) {
+                secEmpty = "0";
+            }
+            if (milliseconds < 10) {
+                msecEmpty = "0";
+            }
+
+            String timerText = "00:" + minEmpty + minutes + ":" + secEmpty + seconds + "." + msecEmpty + milliseconds;
+            timerLabel.setText(timerText);
+        });
+    }
+
+    /**
+     * Handles start action when the start button is pressed
+     */
+    public void startAction() {
+        timer.startUITimer();
+    }
+
+    /**
+     * Handles stop action when the stop button is pressed
+     */
+    public void stopAction() {
+        timer.stopUITimer();
+    }
 }
