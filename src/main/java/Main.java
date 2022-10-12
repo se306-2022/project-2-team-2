77a900ac88dcc6c1c1f408af1cbce71272cf2fa5
Brendan-Z <79791084+Brendan-Z@users.noreply.github.com>
2022-10-12 23:13:44 +1300

    Added CLI for BnBParallel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,24 +25,22 @@ public class Main {
 
         Graph graph = IOParser.read(commands.getInputFile());
 
-//        if (isParallel) {
-//            int numParallelCores = -1;
-//            try {
-//                numParallelCores = commands.getNumParallelCores();
-//            } catch (IllegalAccessException e) {
-//                e.printStackTrace();
-//            }
-//            if (numParallelCores > 1) {
-//                BranchAndBoundParallel algorithmParallel = new BranchAndBoundParallel(graph, numParallelCores);
-//                algorithmParallel.run();
-//                // TODO: Return best schedule for bnb parallel when Matthew finishes.
-//                return algorithmParallel;
-//            }
-//        }
+        if (isParallel) {
+            int numParallelCores = -1;
+            try {
+                numParallelCores = commands.getNumParallelCores();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            if (numParallelCores > 1) {
+                BranchAndBoundParallel algorithmParallel = new BranchAndBoundParallel(graph, numParallelCores);
+                algorithmParallel.run();
+                return algorithmParallel.getBestSchedule();
+            }
+        }
 
         BranchAndBound algorithmSequential = new BranchAndBound(graph, commands.getNumProcessors());
         algorithmSequential.run();
-
         return algorithmSequential.getBestSchedule();
     }
 }
